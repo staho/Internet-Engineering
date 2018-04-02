63f99c2aba4eabcd9d5b222ce6d928231944018d
staho <stacho688@gmail.com>
2018-04-02 16:16:45 +0200

    Adding tournament progress

--- server/controller/tournamentController.js
+++ server/controller/tournamentController.js
@@ -8,7 +8,40 @@ exports.allTournaments = () => {
 
 }
 
-exports.createTournament = () => {
+exports.createTournament = (req, res) => {
+    if(req.body.usernames) {
+        let newTournament = Object.assign({}, req.body)
+
+        newTournament.creator = req.user._id
+
+        User.find()
+            .where("username")
+            .in(newTournament.usernames)
+            .exec((err, records) => {
+                if(err || !records || records.length !== req.body.usernames.length){
+
+                    let diffs = newTournament.usernames.filter(o1 => records.filter(o2 => o2.username === o1).length === 0);
+                    
+                    res.status(400).send({
+                        msg: "Couldn't find usernames in database",
+                        unrecognisedUsernames: diffs
+                    })
+                } else {
+                    newTournament.users = records.map(element => { return element._id})
+
+                    if(newTournament.users.length % 2 === 0) {
+
+                    } else {
+                        //this guy should play dwo duels
+                        let randomUser = newTournament.users[Math.floor(Math.random() * newTournament.users.length)]
+                        
+                    }
+
+                }
+                console.log(newTournament)
+            })
+
+    }
 
 }
 
